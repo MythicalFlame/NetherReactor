fcbc7b82107792a2fa04e4cb9c91d94c4140df9c
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2025-12-19 21:41:30 -0500

    Add ExpandableTag

---
+++ src/main/java/me/mythicalflame/netherreactor/content/ExpandableTag.java
@@ -0,0 +1,101 @@
+package me.mythicalflame.netherreactor.content;
+
+import net.kyori.adventure.key.Key;
+import org.bukkit.Material;
+
+import javax.annotation.Nonnull;
+import java.util.HashSet;
+
+/**
+ * A tag type with built in sets that allow other plugins to add to your tag.
+ */
+public abstract class ExpandableTag extends ModdedTag
+{
+    /**
+     * A set of Materials that are a member of the tag.
+     */
+    @Nonnull
+    private final HashSet<Material> MATERIAL_SET = new HashSet<>();
+    /**
+     * A set of ModdedItems that are a member of the tag.
+     */
+    @Nonnull
+    private final HashSet<ModdedItem> MODDED_ITEM_SET = new HashSet<>();
+    /**
+     * A set of Keys that are a member of the tag.
+     */
+    @Nonnull
+    private final HashSet<Key> KEY_SET = new HashSet<>();
+
+    /**
+     * Constructs a ModdedTag object.
+     *
+     * @param key The non-null key containing the namespace and ID of the tag. The namespace must match your mod's namespace.
+     */
+    public ExpandableTag(@Nonnull Key key)
+    {
+        super(key);
+    }
+
+    /**
+     * Intended for checking whether or not vanilla items are in the tag.
+     *
+     * @param material The material to check for membership.
+     * @return Whether or not the material is a member of this tag.
+     */
+    public boolean isMember(Material material)
+    {
+        return MATERIAL_SET.contains(material);
+    }
+
+    /**
+     * Intended for checking whether or not NetherReactor items are in the tag.
+     *
+     * @param item The item to check for membership.
+     * @return Whether or not the item is a member of this tag.
+     */
+    public boolean isMember(ModdedItem item)
+    {
+        return MODDED_ITEM_SET.contains(item);
+    }
+
+    /**
+     * Intended for checking whether or not other custom items (Nexo, ItemsAdder, etc.) are in the tag.
+     *
+     * @param key The key to check for membership.
+     * @return Whether or not the key is a member of this tag.
+     */
+    public boolean isMember(Key key)
+    {
+        return KEY_SET.contains(key);
+    }
+
+    /**
+     * Adds a Material as a member of the tag.
+     *
+     * @param material The Material to add to this tag.
+     */
+    public void addMaterial(@Nonnull Material material)
+    {
+        MATERIAL_SET.add(material);
+    }
+
+    /**
+     * Adds a ModdedItem as a member of the tag.
+     *
+     * @param item The ModdedItem to add to this tag.
+     */
+    public void addModdedItem(@Nonnull ModdedItem item)
+    {
+        MODDED_ITEM_SET.add(item);
+    }
+    /**
+     * Adds a Key as a member of the tag.
+     *
+     * @param key The Key to add to this tag.
+     */
+    public void addKey(@Nonnull Key key)
+    {
+        KEY_SET.add(key);
+    }
+}
