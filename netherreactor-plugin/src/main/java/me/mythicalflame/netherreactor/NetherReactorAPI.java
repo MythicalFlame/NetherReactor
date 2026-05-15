94a17394c16b224b1efa4a2754abe510dfe1200b
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2026-05-14 22:39:09 -0400

    Add getItemType API

--- netherreactor-plugin/src/main/java/me/mythicalflame/netherreactor/NetherReactorAPI.java
+++ netherreactor-plugin/src/main/java/me/mythicalflame/netherreactor/NetherReactorAPI.java
@@ -1,7 +1,10 @@
 package me.mythicalflame.netherreactor;
 
 import net.kyori.adventure.key.Key;
+import org.bukkit.Registry;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemType;
 
 import javax.annotation.Nonnull;
 
@@ -22,5 +25,27 @@ public final class NetherReactorAPI
     public static void awardStatistic(@Nonnull Player player, @Nonnull Key key, int amount)
     {
         InternalsManager.getStatisticMutator().awardStatistic(player, key, amount);
+    }
+
+    /**
+     * Returns the true ItemType for a stack.
+     *
+     * @param stack The stack to get the ItemType for.
+     * @return The true ItemType for this stack, or null if it cannot find it for some reason.
+     */
+    public static ItemType getItemType(ItemStack stack)
+    {
+        return Registry.ITEM.get(InternalsManager.getItemMutator().getMaterialKey(stack));
+    }
+
+    /**
+     * Returns the true ItemType for a stack.
+     *
+     * @param stack The stack to get the ItemType for.
+     * @return The true ItemType for this stack. If it cannot be found, throws an exception.
+     */
+    public static ItemType getItemTypeOrThrow(ItemStack stack)
+    {
+        return Registry.ITEM.getOrThrow(InternalsManager.getItemMutator().getMaterialKey(stack));
     }
 }
